cb6b02f993ff36fe930c8f1bcf7b65e1030194d7
renso3x <enso123456@gmail.com>
2020-06-22 22:04:02 +0800

    add model address, product

--- app/core/models.py
+++ app/core/models.py
@@ -1,13 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
-                                      PermissionsMixin
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+from django.conf import settings
 
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         """ Create and saves a new user """
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
 
         user = self.model(email=self.normalize_email(email), **extra_fields)
         # encrypt the password
@@ -31,6 +35,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """ Custom user model supports email instead of username """
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -38,4 +43,29 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
 
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
+
+
+class Address(models.Model):
+    """Address attributes"""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+
+    def __str__(self):
+        return self.address
+
+
+class Product(models.Model):
+    """ Product attributes"""
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,38 +1,60 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email="test@refluens.com", password="testpass"):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test create a new user with an email is successfull"""
         email = "test@refluens.com"
-        password = 'ab123'
+        password = "ab123"
 
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password
-        )
+        user = get_user_model().objects.create_user(email=email, password=password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
         """Test the email for new user is normalize"""
-        email = 'romeo@REFLUENS.COM'
-        user = get_user_model().objects.create_user(email, 'testpass')
+        email = "romeo@REFLUENS.COM"
+        user = get_user_model().objects.create_user(email, "testpass")
 
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
         """ Test creating user with no email raises error """
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'qwerqwer')
+            get_user_model().objects.create_user(None, "qwerqwer")
 
     def test_create_new_superuser(self):
         """ Test creating a new superuser"""
-        user = get_user_model().objects.create_superuser(
-            'admin@admin.com',
-            '123123'
-        )
+        user = get_user_model().objects.create_superuser("admin@admin.com", "123123")
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_address_str(self):
+        """Test address string representaion"""
+        my_address = models.Address.objects.create(
+            user=sample_user(),
+            address="Block 2, Lot 13, Phase 3, Ashton Fields South",
+            city="Calamba City",
+            latitude=14.204780,
+            longitude=121.154716,
+        )
+
+        self.assertEqual(str(my_address), my_address.address)
+
+    def test_product_str(self):
+        """Test product string representation"""
+        product = models.Product.objects.create(
+            user=sample_user(),
+            name="Iphone 7 Plus",
+            description="Jet black variant Iphone7+",
+            price=99.99,
+        )
+        self.assertEqual(str(product), product.name)
